62f9893e3d22c3b5cba90fd71b9699e96db04e91
Huy Nguyen <huy.m.nguyen@live.com>
2019-09-03 18:21:30 -0700

    Created burger_db in schema.sql. Inserted 5 burger data into seeds.sql.

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database burgers_db and specified it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table plans.
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+-- Initial burger data.
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whiskey River BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('A.I. Peppercorn Burger');
+INSERT INTO burgers (burger_name) VALUES ('Sauteed Shroom Burger');
