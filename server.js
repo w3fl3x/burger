accd38b31a826e4e250c32f167ceeb1d3595ce54
Huy Nguyen <huy.m.nguyen@live.com>
2019-09-03 22:30:22 -0700

    Add var for all require in server.js. Add app.use and app.listen to server.js.

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller');
+
+app.use('/', routes);
+app.use('/add', routes);
+
+app.listen(port, function() {
+    console.log('Connected on port: ' + port);
+});
