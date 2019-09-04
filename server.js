6c27e77a920d30034fb3eb4c7bddd3c1850727df
Huy Nguyen <huy.m.nguyen@live.com>
2019-09-04 00:06:26 -0700

    Add background image to image folder. Add / in front of public route in server.js. Fix spelling for stylesheet in main.handlebars. Changed spacing for burger row to have it centered.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static('/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
