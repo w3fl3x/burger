42cb0272f2dd82b546c8779fd50f6149edb6fc04
Huy Nguyen <huy.m.nguyen@live.com>
2019-09-03 21:22:12 -0700

    Add orm for all, create and update function.

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,23 @@
 var orm = require('../config/orm');
 
+var burgers = {
+    all: function(cb) {
+        orm.selectAll('burgers', function(res) {
+            cb(res);
+        });
+    },
+    
+    create: function(value, cb) {
+        orm.insertOne('burgers', 'burger_name', value, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(id, devoured, cb) {
+        orm.updateOne('burgers', id, devoured, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burgers;
