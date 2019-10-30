c836c0e084a27badf4309543c857c872d2432e24
Huy Nguyen <huy.m.nguyen@live.com>
2019-10-29 23:58:45 -0700

    Fix spelling in orm.js, burger_controller.js and burger.js.

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,25 @@
-var orm = require('../config/orm');
+var orm = require('../config/orm.js');
 
 var burgers = {
-    all: function(cb) {
-        orm.selectAll('burgers', function(res) {
-            cb(res);
-        });
-    },
-    
-    create: function(value, cb) {
-        orm.insertOne('burgers', 'burger_name', value, function(res) {
-            cb(res);
-        });
-    },
+  all: function(cb) {
+    orm.selectAll('burgers', function(res) {
+      cb(res);
+    });
+  },
 
-    update: function(id, devoured, cb) {
-        orm.updateOne('burgers', id, devoured, function(res) {
-            cb(res);
-        });
-    }
+  create: function(value, cb) {
+    orm.insertOne('burgers', 'burger_name', value, function(res) {
+      console.log(res);
+      cb(res);
+    });
+  },
+
+  update: function(id, devoured, cb) {
+    orm.update("burgers", id, devoured, function(res) {
+      console.log(res);
+      cb(res);
+    });
+  }
 };
 
 module.exports = burgers;
--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 var methodOverride = require('method-override');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static('./public'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 
-var routes = require('./controllers/burgers_controller');
+var routes = require('./controllers/burgers_controller.js');
 
-app.use('/', routes);
-app.use('/add', routes);
+app.use(routes);
+app.use("/", routes);
+app.use("/add", routes);
 
 app.listen(port, function() {
-    console.log('Connected on port: ' + port);
+console.log("Connected on port: " + port);
 });
